0a4157442a5d48f6adc8a30e7b249fabf579be40
StastnyJ <xstastnyj@gmail.com>
2020-09-23 12:13:40 +0200

    basic message

--- src/main/java/com/bettingScanner/api/WebSecurityConfig.java
+++ src/main/java/com/bettingScanner/api/WebSecurityConfig.java
@@ -3,11 +3,11 @@ package com.bettingScanner.api;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+// import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
-@EnableWebSecurity
+// @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
