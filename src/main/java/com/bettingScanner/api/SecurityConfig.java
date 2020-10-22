e1d011bfbfc1c0233c6e281d6cb4a21385c2040e
StastnyJ <xstastnyj@gmail.com>
2020-10-22 22:29:33 +0200

    basic message

--- src/main/java/com/bettingScanner/api/SecurityConfig.java
+++ src/main/java/com/bettingScanner/api/SecurityConfig.java
@@ -12,10 +12,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
+
         http.authorizeRequests().antMatchers(HttpMethod.OPTIONS, "**").permitAll();
-
         http.authorizeRequests().antMatchers("/**").permitAll();
-
+        http.authorizeRequests().anyRequest().permitAll();
         http.csrf().disable();
     }
 }
