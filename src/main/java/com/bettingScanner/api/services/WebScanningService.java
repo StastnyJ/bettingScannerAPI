9682208404325195c9bd785ca626901275279edf
StastnyJ <xstastnyj@gmail.com>
2021-02-08 06:01:34 +0100

    x

--- src/main/java/com/bettingScanner/api/services/WebScanningService.java
+++ src/main/java/com/bettingScanner/api/services/WebScanningService.java
@@ -63,6 +63,7 @@ public class WebScanningService {
     }
 
     public static String getSiteContent(URL url, List<HttpCookie> cookies, String authorization) {
+        HttpURLConnection.setFollowRedirects(false);
         HttpURLConnection con = null;
         StringBuffer res = new StringBuffer();
         try {
@@ -92,6 +93,7 @@ public class WebScanningService {
     }
 
     public static String getSiteContent(URL url, Map<String, String> jsonBodyParams, List<HttpCookie> cookies) {
+        HttpURLConnection.setFollowRedirects(false);
         HttpURLConnection con = null;
         StringBuffer res = new StringBuffer();
         String bodyString = convertToJson(jsonBodyParams);
